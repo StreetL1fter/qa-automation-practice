70c36e3cd61d508bc28de497884eecbf99e9625c
Ismail <ismailmendgaliev2005@mail.ru>
2026-06-27 07:58:17 +0400

    New

--- tests/api/conftest.py
+++ tests/api/conftest.py
@@ -5,14 +5,17 @@ import allure
 
 load_dotenv()
 
+@pytest.fixture(scope="session")
+def api_base_url():
+    return "https://reqres.in/api"
+
 @pytest.fixture
 def api_headers():
     api_key = os.getenv("REQRES_API_KEY")
     
     if api_key:
-        api_key = api_key.replace('"', '').replace("'", "").strip()
+        api_key = api_key.replace("'", "").replace('"', '').strip()
         
     return {
         "x-api-key": api_key
     }
-
